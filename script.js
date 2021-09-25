c18a29f2ce0e147704fbfdd01ca089c0c8cc4bcb
Dmytro Maksymiv <dmutromacsumiv@ukr.net>
2021-09-25 17:11:12 +0300

    Lesson 2 - Home work

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 'use strict';
 
-
-
 document.querySelector('h1').style.color ='red';
+
+// infinite cycle
+
+// let i = 0;
+// while (true) {
+//   console.log(i++);
+// }
+
+// UpperCase and LowerCase
+
+let str = 'Test'
+const str2 = str.split('').map(word=>word===word.toLowerCase() ? word.toUpperCase() : 
+word.toLowerCase()).join('')
+
+console.log(str2)
